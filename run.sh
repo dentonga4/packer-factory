7d086681e42489bd5c86d41f297e393eaff01781
Gary Denton <gary.denton@contino.io>
2024-10-21 12:24:06 +0100

    Updating runtime execution script

--- run.sh
+++ run.sh
@@ -14,10 +14,11 @@ fi
 (cd terraform/ && terraform init)
 (cd terraform/ && terraform apply --auto-approve=true)
 
+packer build squid_proxy.json
 # Run Packer and build machine images
-for filename in ./*.json; do
-    packer build $filename
-done
+#for filename in ./*.json; do
+#    packer build $filename
+#done
 
 # Remove infrastructure required for Packer builds
 (cd terraform/ && terraform destroy --auto-approve=true)
